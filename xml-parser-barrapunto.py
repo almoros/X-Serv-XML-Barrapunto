cec6ed5aebe44200a9858e94cfb6cc56c0934a75
almoros <javieraf@msn.com>
2018-04-18 11:34:33 +0200

    Versión 2

--- xml-parser-barrapunto.py
+++ xml-parser-barrapunto.py
@@ -14,13 +14,17 @@
 from xml.sax.handler import ContentHandler
 from xml.sax import make_parser
 import sys
+from urllib import request
 
 class myContentHandler(ContentHandler):
 
     def __init__ (self):
+        self.archivo = open("contents.html", "w")
+
         self.inItem = False
         self.inContent = False
         self.theContent = ""
+        self.line = ""
 
     def startElement (self, name, attrs):
         if name == 'item':
@@ -30,43 +34,39 @@ class myContentHandler(ContentHandler):
                 self.inContent = True
             elif name == 'link':
                 self.inContent = True
-            
+
     def endElement (self, name):
         if name == 'item':
             self.inItem = False
         elif self.inItem:
             if name == 'title':
-                line = "Title: " + self.theContent + "."
+                self.line = "<li>Title: " + self.theContent + ".</li>"
                 # To avoid Unicode trouble
-                print (line.encode('utf-8'))
+                print(self.line)
+                self.archivo.write(self.line)
                 self.inContent = False
                 self.theContent = ""
             elif name == 'link':
-                print (" Link: " + self.theContent + ".")
+                self.link = self.theContent
+                links = "<p>Link: <a href='" + self.link + "'>" + self.link + "</a></p>\n"
+                print(links)
+                self.archivo.write(links)
                 self.inContent = False
                 self.theContent = ""
+                self.link = ""
 
     def characters (self, chars):
         if self.inContent:
             self.theContent = self.theContent + chars
-            
-# --- Main prog
 
-if len(sys.argv)<2:
-    print ("Usage: python xml-parser-barrapunto.py <document>")
-    print ()
-    print (" <document>: file name of the document to parse")
-    sys.exit(1)
-    
 # Load parser and driver
-
 theParser = make_parser()
 theHandler = myContentHandler()
 theParser.setContentHandler(theHandler)
 
-# Ready, set, go!
+# Ready, set, go!2  --> con la pagina web original de barrapunto
+url = "http://barrapunto.com/index.rss"
+xmlStream = request.urlopen(url)
+theParser.parse(xmlStream)
 
-xmlFile = open(sys.argv[1],"r")
-theParser.parse(xmlFile)
-
-print ("Parse complete")
+print("Parse complete")
